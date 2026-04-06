c33f22c7b0c23945590f0ba762bffb589abbd63a
Steven Stevenson <stevenstevenson@Mac.localdomain>
2026-04-05 21:58:14 -0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import Nav from './components/Nav'
 import Hero from './components/Hero'
 import About from './components/About'
-import Projects from './components/Projects'
 import Connect from './components/Connect'
 import Footer from './components/Footer'
 
